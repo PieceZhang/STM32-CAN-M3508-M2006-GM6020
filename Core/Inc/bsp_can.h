d9c792f4ebae92eccf757f86220020aeef7914bb
YuelinZhang <2239436414@qq.com>
2021-07-27 15:38:04 +0800

    can debug

--- Core/Inc/bsp_can.h
+++ Core/Inc/bsp_can.h
@@ -32,25 +32,25 @@
 #include "mytype.h"
 #include "can.h"
 
+
 /*CAN发送或是接收的ID*/
 typedef enum
 {
-
 	CAN_2006Moto_ALL_ID = 0x200,
 	CAN_2006Moto1_ID = 0x201,
 	CAN_2006Moto2_ID = 0x202,
 	CAN_2006Moto3_ID = 0x203,
 	CAN_2006Moto4_ID = 0x204,
-	
 }CAN_Message_ID;
+
 
 #define FILTER_BUF_LEN		5
 /*接收到的云台电机的参数结构体*/
 typedef struct{
 	int16_t	 	speed_rpm;
-    float  	real_current;
-    int16_t  	given_current;
-    uint8_t  	hall;
+  float  	real_current;
+  int16_t  	given_current;
+  uint8_t  	hall;
 	uint16_t 	angle;				//abs angle range:[0,8191]
 	uint16_t 	last_angle;	//abs angle range:[0,8191]
 	uint16_t	offset_angle;
@@ -62,16 +62,16 @@ typedef struct{
 	u32			msg_cnt;
 }moto_measure_t;
 
-/* Extern  ------------------------------------------------------------------*/
-extern moto_measure_t  moto_chassis[];
 
+extern moto_measure_t  moto_chassis[];
 
 
 void my_can_filter_init(CAN_HandleTypeDef* hcan);
 void my_can_filter_init_recv_all(CAN_HandleTypeDef* _hcan);
 void can_filter_recv_special(CAN_HandleTypeDef* hcan, uint8_t filter_number, uint16_t filtered_id);
-void get_moto_measure(moto_measure_t *ptr, CAN_HandleTypeDef* hcan);
+void get_moto_measure(moto_measure_t *ptr, uint8_t Data[]);
 void can_receive_onetime(CAN_HandleTypeDef* _hcan, int time);
 void set_moto_current(CAN_HandleTypeDef* hcan, s16 iq1, s16 iq2, s16 iq3, s16 iq4);
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef* hcan);
+
 #endif
--- Core/Src/bsp_can.c
+++ Core/Src/bsp_can.c
@@ -24,12 +24,9 @@
 #include "can.h"
 #include "bsp_can.h"
 
+// list of 4, store the information (in structure 'moto_measure_t') of 4 motors respectively
+moto_measure_t moto_chassis[4] = {0};
 
-moto_measure_t moto_chassis[4] = {0};//4 chassis moto
-//CAN_TxHeaderTypeDef		Tx1Message;	
-
-void get_total_angle(moto_measure_t *p);
-void get_moto_offset(moto_measure_t *ptr, CAN_HandleTypeDef* hcan);
 
 /*******************************************************************************************
   * @Func		my_can_filter_init
@@ -72,6 +69,7 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef* hcan)
 {
 	CAN_RxHeaderTypeDef		Rx1Message;
 	uint8_t Data[8];
+	uint8_t MotorID;
 	
 	Rx1Message.StdId = 0x201;  // 0x200 + CANID    0x200?
 	Rx1Message.ExtId = 0;  // CAN_ID_STD, ExtId is invalid
@@ -87,22 +85,29 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef* hcan)
 
 	HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &Rx1Message, Data);
 	
-	//ignore can1 or can2.
 	switch(Rx1Message.StdId)
 	{
-		case CAN_2006Moto1_ID:
-		case CAN_2006Moto2_ID:
-		case CAN_2006Moto3_ID:
-		case CAN_2006Moto4_ID:
-			{
-//				static u8 i;
-//				i = hcan->pRxMsg->StdId - CAN_2006Moto1_ID;
-//				
-//				get_moto_measure(&moto_chassis[i], hcan);
-			}
-			break;
+		case CAN_2006Moto1_ID: MotorID=0; break;
+		case CAN_2006Moto2_ID: MotorID=1; break;
+		case CAN_2006Moto3_ID: MotorID=2; break;
+		case CAN_2006Moto4_ID: MotorID=3; break;
 	}
 		
+	
+//	get_moto_measure(&moto_chassis[MotorID], Data);
+	/* get_moto_measure (1 motor) */
+	moto_chassis[MotorID].last_angle = moto_chassis[MotorID].angle;
+	moto_chassis[MotorID].angle = (uint16_t)(Data[0]<<8 | Data[1]) ;
+	moto_chassis[MotorID].speed_rpm  = (int16_t)(Data[2]<<8 | Data[3]);
+	moto_chassis[MotorID].real_current = (Data[4]<<8 | Data[5])*5.f/16384.f;
+	moto_chassis[MotorID].hall = Data[6];
+	if(moto_chassis[MotorID].angle - moto_chassis[MotorID].last_angle > 4096)
+		moto_chassis[MotorID].round_cnt --;
+	else if (moto_chassis[MotorID].angle - moto_chassis[MotorID].last_angle < -4096)
+		moto_chassis[MotorID].round_cnt ++;
+	moto_chassis[MotorID].total_angle = moto_chassis[MotorID].round_cnt * 8192 + moto_chassis[MotorID].angle - moto_chassis[MotorID].offset_angle;
+	
+	
 	/*#### add enable can it again to solve can receive only one ID problem!!!####**/
 	__HAL_CAN_ENABLE_IT(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING);
 
@@ -116,16 +121,13 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef* hcan)
   * @Retval		None
   * @Date     2015/11/24
  *******************************************************************************************/
-//void get_moto_measure(moto_measure_t *ptr, CAN_HandleTypeDef* hcan)
+//void get_moto_measure(moto_measure_t *ptr, uint8_t Data[])
 //{
-
 //	ptr->last_angle = ptr->angle;
-//	ptr->angle = (uint16_t)(hcan->pRxMsg->Data[0]<<8 | hcan->pRxMsg->Data[1]) ;
-//	ptr->speed_rpm  = (int16_t)(hcan->pRxMsg->Data[2]<<8 | hcan->pRxMsg->Data[3]);
-//	ptr->real_current = (hcan->pRxMsg->Data[4]<<8 | hcan->pRxMsg->Data[5])*5.f/16384.f;
-
-//	ptr->hall = hcan->pRxMsg->Data[6];
-//	
+//	ptr->angle = (uint16_t)(Data[0]<<8 | Data[1]) ;
+//	ptr->speed_rpm  = (int16_t)(Data[2]<<8 | Data[3]);
+//	ptr->real_current = (Data[4]<<8 | Data[5])*5.f/16384.f;
+//	ptr->hall = Data[6];
 //	
 //	if(ptr->angle - ptr->last_angle > 4096)
 //		ptr->round_cnt --;
@@ -134,12 +136,14 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef* hcan)
 //	ptr->total_angle = ptr->round_cnt * 8192 + ptr->angle - ptr->offset_angle;
 //}
 
+
 /*this function should be called after system+can init */
 //void get_moto_offset(moto_measure_t *ptr, CAN_HandleTypeDef* hcan)
 //{
 //	ptr->angle = (uint16_t)(hcan->pRxMsg->Data[0]<<8 | hcan->pRxMsg->Data[1]) ;
 //	ptr->offset_angle = ptr->angle;
 //}
+
 
 #define ABS(x)	( (x>0) ? (x) : (-x) )
 /**
